53f16482f25faecf7da8d34c3e7daecc44c64894
Maya Gao <maya@stripe.com>
2018-06-28 23:14:49 -0700

    reset css to reduce browser inconsistencies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css/reset.css';
 import './css/tachyons.css';
 import './css/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+import { styles } from "./components/styles";
+import { cssProperties } from "./components/css_properties";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
